eb9ed040146df170c180d45d91836d246307371e
Kevin <sxzz@sxzz.moe>
2022-02-17 09:08:51 +0800

    chore: upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import path from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "~/": `${path.resolve(__dirname, "src")}/`,
+      '~/': `${pathSrc}/`,
     },
   },
   css: {
@@ -24,9 +26,10 @@ export default defineConfig({
     Components({
       resolvers: [
         ElementPlusResolver({
-          importStyle: "sass",
+          importStyle: 'sass',
         }),
       ],
+      dts: path.resolve(pathSrc, 'components.d.ts'),
     }),
   ],
-});
+})
