6c532a8a87bca142e2d920d30982e0dacda0a9a9
YunYouJun <me@yunyoujun.cn>
2022-03-17 20:40:28 +0800

    fix: add default font for body

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,10 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--el-text-color-primary);
 }
+
 .element-plus-logo {
   width: 50%;
 }
