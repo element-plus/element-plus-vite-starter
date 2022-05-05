639ca5996d4b89befab7b0cb48174a719dc89d35
YunYouJun <me@yunyoujun.cn>
2022-05-05 19:24:41 +0800

    feat: add dark mode

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import { toggleDark } from '~/composables';
+</script>
+
 <template>
   <el-menu class="el-menu-demo" mode="horizontal">
     <el-menu-item index="1">Element Plus</el-menu-item>
@@ -15,5 +19,10 @@
     </el-sub-menu>
     <el-menu-item index="3" disabled>Info</el-menu-item>
     <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item h="full" @click="toggleDark()">
+      <button class="border-none w-full bg-transparent cursor-pointer" style="height: var(--el-menu-item-height);">
+        <i inline-flex i="dark:ep-moon ep-sunny" />
+      </button>
+    </el-menu-item>
   </el-menu>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import Unocss from 'unocss/vite'
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
 const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
@@ -34,6 +43,23 @@ export default defineConfig({
         }),
       ],
       dts: 'src/components.d.ts',
+    }),
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+          warn: true,
+        }),
+      ],
+      transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+      ]
     }),
   ],
 })
