089dfef5b53291401b02e9786fe4100dfd90b940
YunYouJun <me@yunyoujun.cn>
2023-02-19 06:25:40 +0800

    feat: adjust example layout style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,18 +24,31 @@ const toast = () => {
   </p>
 
   <!-- example components -->
-  <el-button @click="toast">El Message</el-button>
-  <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
-  <el-button type="success" @click="count++">count is: {{ count }}</el-button>
-  <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
-  <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
-  <el-button type="info" @click="count++">count is: {{ count }}</el-button>
-  <br />
-  <el-input v-model="input" style="width: 200px; margin: 20px" />
-  <el-tag>Tag 1</el-tag>
+  <div class="mb-4">
+    <el-button size="large" @click="toast">El Message</el-button>
+  </div>
+  
+  <div class="my-2 text-center flex flex-wrap justify-center items-center">
+    <el-button @click="count++">count is: {{ count }}</el-button>
+    <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
+    <el-button type="success" @click="count++">count is: {{ count }}</el-button>
+    <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
+    <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
+    <el-button type="info" @click="count++">count is: {{ count }}</el-button>
+  </div>
 
-  <br />
-  <el-date-picker v-model="curDate" type="date" placeholder="Pick a day"></el-date-picker>
+
+  <div>
+    <el-tag type="success" class="m-1">Tag 1</el-tag>
+    <el-tag type="warning" class="m-1">Tag 1</el-tag>
+    <el-tag type="danger" class="m-1">Tag 1</el-tag>
+    <el-tag type="info" class="m-1">Tag 1</el-tag>
+  </div>
+
+  <div class="my-2">
+    <el-input class="m-2" v-model="input" style="width: 200px;" />
+    <el-date-picker class="m-2" v-model="curDate" type="date" placeholder="Pick a day"></el-date-picker>
+  </div>
 
   <p>For example, we can custom primary color to 'green'.</p>
 
@@ -64,6 +77,10 @@ const toast = () => {
 
 <style>
 .ep-button {
+  margin: 4px;
+}
+.ep-button + .ep-button {
+  margin-left: 0;
   margin: 4px;
 }
 </style>
